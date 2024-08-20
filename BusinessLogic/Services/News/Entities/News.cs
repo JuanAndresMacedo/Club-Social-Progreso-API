4d302560b2c7caad2024aaa5941163c1fa609cde
JuanAndresMacedo <juanmacedo2003@hotmail.com>
2024-08-19 22:18:52 -0300

    Primer commit

---
+++ BusinessLogic/Services/News/Entities/News.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.News.Entities
+{
+    public sealed record class News
+    {
+        public string Id { get; init; }
+
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+
+        public string Date { get; set; } = null!;
+
+        public string Topic { get; set; } = null!;
+
+        public bool VerticalImage { get; set; }
+
+        public News()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ BusinessLogic/Services/News/INewsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services.News
+{
+    public interface INewsService
+    {
+        List<Entities.News> GetAll(Expression<Func<Entities.News, bool>> predicate);
+
+        List<Entities.News> GetMainNews(Expression<Func<Entities.News, bool>> predicate);
+
+        List<Entities.News> GetSecondaryNews(Expression<Func<Entities.News, bool>> predicate);
+    }
+}
