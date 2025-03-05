da4ea66cab8cfb647d87a43ca4ee11fee33243bb
JuanAndresMacedo <juanmacedo2003@hotmail.com>
2025-03-05 13:36:59 -0300

    Nuevo repo prueba 1

--- BusinessLogic/Services/NewsFolder/NewsService.cs
+++ BusinessLogic/Services/NewsFolder/NewsService.cs
@@ -7,10 +7,35 @@ namespace BusinessLogic.Services.NewsFolder;
 public class NewsService : INewsService
 {
     private readonly IRepository<News> _repository;
-    
+    private readonly List<News> _allNews = new List<News>();
+
     public NewsService(IRepository<News> repository)
     {
-        _repository = repository;
+        //_repository = repository;
+        _allNews.Add(
+            new News
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Noticia de Ejemplo 1",
+                Description = "Esta es la primera noticia de ejemplo",
+                ImageUrl = "https://misitio.com/imagen1.jpg",
+                Date = "01/03/2025",
+                Topic = "Tecnología",
+                VerticalImage = false,
+                Content = new List<NewsContent>
+            {
+                new NewsContent
+                {
+                    Paragraph = "Párrafo 1 de la noticia 1",
+                    Index = 0
+                },
+                new NewsContent
+                {
+                    Paragraph = "Párrafo 2 de la noticia 1",
+                    Index = 1
+                }
+            }
+            });
     }
 
     public News GetById(string newsId)
@@ -22,10 +47,11 @@ public class NewsService : INewsService
 
     public List<News> GetAll(Expression<Func<News, bool>> predicate)
     {
-        List<News> allNews = _repository.GetAll(predicate)
+        /*List<News> allNews = _repository.GetAll(predicate)
             .OrderByDescending(news => convertirStringAFecha(news.Date))
-            .ToList();
-        
+            .ToList();*/
+        List<News> allNews = _allNews.ToList();
+
         foreach (var news in allNews)
         {
             news.Content = news.Content.OrderBy(c => c.Index).ToList();
